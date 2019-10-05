116abbec7cdfd0215ec249a3a2fa62ea031b7a4c
Araknor99 <eli.lehnert@gmx.de>
2019-10-05 16:00:22 +0200

    working on player movement

--- SWTU/scenes/plastic_spawns.gd
+++ SWTU/scenes/plastic_spawns.gd
@@ -2,14 +2,12 @@ extends Node
 
 var windowSizeY = OS.get_real_window_size().y
 var windowSizeX = OS.get_real_window_size().x
-
 var spawnY = windowSizeY - windowSizeY/6
-var spawnOffsetX = windowSizeX/6
 
 func _ready():
 	var children = get_children()
-	for i in range(3):
-		var spawnX = windowSizeX/3 * i + spawnOffsetX 
-		var offset = Vector2(spawnX,spawnY)
+	
+	for i in range(get_parent().railcount):
+		var spawnX = windowSizeX/3 * i + get_parent().offsetX
+		var offset = Vector2(spawnY,spawnX)
 		children[i].translate(offset)
-	pass 
--- SWTU/scripts/PlasticLevel.gd
+++ SWTU/scripts/PlasticLevel.gd
@@ -1,12 +1,10 @@
 extends "res://scripts/Level.gd"
 
 export var railcount = 3
-var rails = []
 
 var windowSizeX = OS.get_real_window_size().x
 var windowSizeY = OS.get_real_window_size().y
-var offsetX = windowSizeX/(railcount*2)
+export var offsetX: float = windowSizeX/(railcount*2)
 
 func _ready():
-	for i in range(railcount):
-		rails.append(windowSizeX/3 * i + offsetX)
+	pass
