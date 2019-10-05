70b5a0026f3018afe219771f94abd05f05c7e634
Mastermori <mastermoriii@gmail.com>
2019-10-05 15:15:08 +0200

    Added Global methods for difficulty and time

--- SWTU/scenes/Globals.gd
+++ SWTU/scenes/Globals.gd
@@ -1,2 +1,14 @@
 extends Node
 
+export var lives = 3
+export var time_modifier = 1
+export var round_counter = 0
+
+func round_finished():
+	round_counter += 1
+
+func get_time_modifier():
+	return pow(0.95, round_counter)
+
+func get_difficulty():
+	return 10 + round_counter * 10 + pow(round_counter, 1.3)
