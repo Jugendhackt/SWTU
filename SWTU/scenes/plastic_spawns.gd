3e570eddba730b26bad85fb93baa6e628268e718
Araknor99 <eli.lehnert@gmx.de>
2019-10-05 14:16:35 +0200

    added spawn positions for player and plastic bags

---
+++ SWTU/scenes/plastic_spawns.gd
@@ -0,0 +1,16 @@
+extends Node
+
+var windowSizeY = OS.get_real_window_size().y
+var windowSizeX = OS.get_real_window_size().x
+
+var spawnY = windowSizeY - windowSizeY/6
+var spawnOffsetX = windowSizeX/6
+
+func _ready():
+	var children = get_children()
+	for i in range(3):
+		var spawnX = windowSizeX/3 * i + spawnOffsetX 
+		var offset = Vector2(spawnX,spawnY)
+		children[i].translate(offset)
+		print(spawnX)
+	pass 
