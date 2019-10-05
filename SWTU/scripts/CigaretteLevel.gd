833c4b70cbc81f6d52e6edd012b3224d2397821b
Mastermori <mastermoriii@gmail.com>
2019-10-05 20:31:27 +0200

    added scene change functionality and timer bar

--- SWTU/scripts/CigaretteLevel.gd
+++ SWTU/scripts/CigaretteLevel.gd
@@ -1,6 +1,6 @@
 extends "res://scripts/Level.gd"
 
-onready var badboy = load("res://scenes/Cigarette/BadBoy.tscn")
+onready var badboy = load("res://scenes/BadBoy.tscn")
 const dirs = ["Up", "Left", "Down", "Right"]
 
 func _ready():
@@ -11,5 +11,3 @@ func random_bboy():
 	var bbinst = badboy.instance()
 	get_node("Spawn" + dirs[rand_range(0, dirs.size())]).add_child(bbinst)
 
-func start_level(time):
-	timer.start(time)
